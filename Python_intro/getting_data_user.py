7b7fa25433ec64d8504d7c0d0403074d7d66036d
jorgezafra94 <jorgezafra94@hotmail.com>
2020-12-19 14:55:01 -0500

    input from users

---
+++ Python_intro/getting_data_user.py
@@ -0,0 +1,15 @@
+# how can we get data from our users
+user_name = input('please give us your name: ')
+my_name = 'Jorge'
+
+print(f'Hi this is my name {my_name} nice to meet you {user_name}')
+
+# this is one important thing
+a = 'a' * 5
+print(a)
+
+# in this way we can change the type from a string to a integer
+age = input('please give us your age: ')
+age = int(age)
+
+print(f'Nice your age is {age}')
