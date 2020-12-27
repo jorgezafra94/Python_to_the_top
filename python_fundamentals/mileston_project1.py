189005c30900c66335ee925e2fdae242dd7d2b6d
jorgezafra94 <jorgezafra94@hotmail.com>
2020-12-27 11:13:12 -0500

    milestone project with best practices

--- python_fundamentals/mileston_project1.py
+++ python_fundamentals/mileston_project1.py
@@ -6,37 +6,52 @@ movies = []
 
 menu = """
     SELECT AN OPTION
-    1. add a movie
-    2. print all movies
-    3. find a movie
+    a - add a movie
+    p - print all movies
+    f - find a movie
 
-    'q' to terminate
+    q - to terminate
 """
 option = input(menu)
 
-while (option != 'q'):
-    if option == '1':
-        movie = {}
-        name = input('insert the title of the movie: ')
-        director = input('insert the director: ')
-        year = input('insert the release year: ')
-        movie['name'] = name
-        movie['director'] = director
-        movie['year'] = year
+def add_mov():
+    movie = {}
+    name = input('insert the title of the movie: ')
+    director = input('insert the director: ')
+    year = input('insert the release year: ')
+    movie['name'] = name
+    movie['director'] = director
+    movie['year'] = year
 
-        movies.append(movie)
+    movies.append(movie)
 
-    elif option == '2':
-        for movie in movies:
-            print(f"Name: {movie['name']}, Director: {movie['director']}, Year: {movie['year']}")
 
+def print_mov():
+    for movie in movies:
+        print(f"Name: {movie['name']}, Director: {movie['director']}, Year: {movie['year']}")
+
+
+def find_mov():
+    name = input('insert the name of the movie to find: ')
+    for movie in movies:
+        if movie['name'].lower() == name.lower():
+            print(f'Name: {movie["name"]}, Director: {movie["director"]}, Year: {movie["year"]}')
+            break
     else:
-        name = input('insert the name of the movie to find: ')
-        for movie in movies:
-            if movie['name'].lower() == name.lower():
-                print(f'Name: {movie["name"]}, Director: {movie["director"]}, Year: {movie["year"]}')
-                break
-        else:
-            print('this movie is not listed, if you want you can add it')
+        print('this movie is not listed, if you want you can add it')
 
+
+options = {
+    'a': add_mov,
+    'p': print_mov,
+    'f': find_mov
+}
+
+
+while (option != 'q'):       
+    if option in options.keys():
+        options[option]()
+    else:
+        print('please enter a valid option')
+        
     option = input(menu) 
